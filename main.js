319a8871679b36adfe098e3ae4e71725276d46f7
Benjamin Springer <benjamin.springer98@gmail.com>
2021-04-25 21:44:02 +0200

    added decimal point and negative functionality

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const numberButtons = document.querySelectorAll(".number");
 const operationButtons = document.querySelectorAll(".operation");
 const equalsButton = document.querySelector("#equals");
 const deleteButton = document.querySelector("#delete");
+const decimalPointButton = document.querySelector("#decimalPoint");
+const negativeButton = document.querySelector("#negative");
 const clearButton = document.querySelector("#clear");
 const display = document.querySelector("#display");
 
@@ -19,9 +21,17 @@ operationButtons.forEach((operationButton) => {
 equalsButton.addEventListener("click", check);
 deleteButton.addEventListener("click", deleteLastDigit);
 clearButton.addEventListener("click", reset);
+decimalPointButton.addEventListener("click", appendDecimalPoint);
+negativeButton.addEventListener("click", makeNegative);
 
 
-function appendNumber() {
+function appendNumber() { 
+    if(display.textContent === "" || display.textContent === "-" && this.value === "0") { // don't allow 0 as first number
+        return;
+    }
+    if(display.textContent.length > 11) { // don't allow too large inputs
+        return;
+    }
     display.append(this.value);
 }
 
@@ -47,14 +57,33 @@ function deleteLastDigit() {
     display.textContent = currentNumber.replace(currentNumber[currentNumber.length-1], "");
 }
 
+function appendDecimalPoint() {
+    if(display.textContent.includes(".")) { // return if there's currently a decimal point in the display
+        return;
+    }
+    display.textContent = display.textContent + ".";
+}
+
+function makeNegative() {
+    if(display.textContent[0] === "-") { // reverse negation, if currently displayed number is already negative
+        display.textContent = display.textContent.replace(display.textContent[0], "");
+        return;
+    }
+    display.textContent = "-" + display.textContent;
+}
 
 function check() {
     secondNumber = display.textContent;
+
+    if(currentOperation === null) {
+        return;
+    }
     if (currentOperation === "divide" && secondNumber === "0") {
         display.textContent = "Err: divide by 0";
         return;
     }
     display.textContent = operate(currentOperation, Number(firstNumber), Number(secondNumber));
+    currentOperation = null;
 }
 
 
